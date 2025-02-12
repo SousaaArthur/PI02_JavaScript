31a1f08339c34e50e4958f52acd30c5001f5eb47
ARTHUR ARAUJO SOUSA <arthur.asousa@senacsp.edu.br>
2025-02-11 21:29:59 -0300

    Aula 01 - JavaScript

---
+++ Aula01/login.js
@@ -0,0 +1,8 @@
+let login = "admin";
+let password = 1234;
+
+if (login === "admin" && password === "1234"){
+  console.log("Login acessado com sucesso");
+} else {
+  console.error("Login ou senha incorreto!!!")
+}
